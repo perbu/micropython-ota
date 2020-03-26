d796f15ff3147d2bbe9f434be9457baa0aa4b22d
Per Buer <per.buer@gmail.com>
2020-03-26 18:27:34 +0100

    works

---
+++ main/blinky.py
@@ -0,0 +1,22 @@
+import machine
+
+class BlinkyLed:
+
+    def toggle_state(self,what):
+        if self.pin.value():
+            self.pin.off()
+        else:
+            self.pin.on()
+
+    def __init__(self):
+        pin = machine.Pin(2, machine.Pin.OUT)
+        pin.off()
+        self.pin = pin
+        tim = machine.Timer(-1)
+        self.tim = tim
+
+    def enable(self):
+        self.tim.init(period=1000, mode=machine.Timer.PERIODIC, callback=self.toggle_state)
+
+    def disable(self):
+        self.tim.deinit()
